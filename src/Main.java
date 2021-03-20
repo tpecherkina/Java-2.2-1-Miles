bd16f61d0fa6a7151b82394a901e8cac833d5bc1
tpecherkina <tpecherkina18@gmail.com>
2021-03-20 12:04:14 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long bonusForMiles = 20;
+        long miles = 1000;
+        long bonusAmount = bonusForMiles*miles;
+        System.out.println(bonusAmount);
+    }
+}
